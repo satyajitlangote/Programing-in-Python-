398962cd38f701f9278a54602ca9f386609f917f
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-01-29 08:19:26 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,20 @@
+# Empty Dictionary
+Dict={ }
+print("Empty Dictionary ....!")
+print(Dict)
+# create dict with integer keys
+Dict={1:'Geeks',2:'For',3:'Geeks'}
+print("Dictionary use of integer keys ")
+print(Dict)
+# create Dict with mixed key
+Dict={'Name':'Geeks', 1:[1,2,3,4]}
+print("Dictionary with mixed keys ")
+print(Dict)
+#Create Dict with dict() method
+Dict={1:'Geeks',2:'For',3:'Geeks'}
+print("Use of dictionary with use of dictionary  ")
+print(Dict)
+#Create Dict with each items as a pair
+Dict=dict([(1,'Geeks'),(2,'For'),(3,'Geeks')])
+print("Dictionary with each items as a pair ")
+print(Dict)
