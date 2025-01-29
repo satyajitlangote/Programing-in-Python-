a0c5a8eab0eeef6a5949f14ba436aa14c555aa50
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-01-29 08:26:30 +0530

    Add files via upload

---
+++ AirthmeticOpt.py
@@ -0,0 +1,20 @@
+a=6
+b=10
+#Addition
+add=a+b
+print(add)
+#Substraction
+sub=a-b
+print(sub)
+#Multiplication
+mult=a*b
+print(mult)
+#Division
+div=a/b
+print(div)
+#Division of (floor) of number
+div2=a//b
+print(div2)
+#power
+p=a**b
+print(p)
