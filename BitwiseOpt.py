8b9f89403dba199c049fb31c25705cb7a309c814
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-01-29 08:48:55 +0530

    Add files via upload

---
+++ BitwiseOpt.py
@@ -0,0 +1,14 @@
+a=20
+b=40
+# Bitwise & operator
+print(a & b)
+# Bitwise | operator
+print(a | b)
+# Bitwise ~ not operator
+# print(a ~ b)
+# Bitwise ^ xor operator
+print(a ^ b)
+# Bitwise >> right shift operator
+print(a >> b)
+# Bitwise << left shift operator
+print(a << b)
