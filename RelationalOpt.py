88113ea10e051b72c87f9e49d7a78f980919da07
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-01-29 08:31:51 +0530

    Add files via upload

---
+++ RelationalOpt.py
@@ -0,0 +1,16 @@
+a=10
+b=20
+# a > b is false
+print(a>b)
+# a < b is true
+print(a < b)
+# a == b is False
+print(a==b)
+# a != b is True 
+print(a != b)
+# a >= b is True
+print(a <= b)
+# a is b
+print(a is b)
+# a is not b
+print(a is not b)
