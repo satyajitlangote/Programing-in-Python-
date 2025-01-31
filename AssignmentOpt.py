679d3e1adb3aa2515c383728f92d78809d367ed5
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-01-31 23:14:30 +0530

    Add files via upload

---
+++ AssignmentOpt.py
@@ -0,0 +1,17 @@
+a=10
+#Assign Value
+b=a
+print(b)
+# add and assign value
+b+=a
+print(b)
+# Substract and assign value
+b-=a
+print(b)
+# Multiply and assigb value
+b*=a
+print(b)
+# Bitwise left shift opt
+b <<=a
+print(b)
+
