235494a3d8fca90854d4a8efb23094504d08ef7c
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-04 21:27:24 +0530

    Add files via upload

---
+++ MemebershipOpt.py
@@ -0,0 +1,12 @@
+x=24
+y=20
+list=[10,20,30,40,50]
+if(x not in list)
+    print("X is not present in given ")
+else:
+    print(" X is present Given list ")
+    if(y not in list )
+        print("Y is present in given list")
+    else:
+        print("y is not present in given ")
+        
