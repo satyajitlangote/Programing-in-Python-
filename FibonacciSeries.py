9430e6b619908a10395922ac8d46ddaf1aa792b9
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-04 21:24:41 +0530

    Add files via upload

---
+++ FibonacciSeries.py
@@ -0,0 +1,8 @@
+## Writng Fibonacci Series
+fib=[0,1]
+# Range starts from 0 by default
+n=5
+for i in range(n):
+    fib.append(fib[-1]+ fib[-2])
+# Converting the list of integers to string
+print(','.join(str(e)for e in fib))
