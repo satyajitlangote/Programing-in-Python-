360f540678c9daefe71398fc0fe8a0e86c23706f
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-19 19:45:22 +0530

    Add files via upload

---
+++ Second1.py
@@ -0,0 +1,16 @@
+#2. # Program to Accept two number and display addition, subtraction, 
+#multiplication, division and modules. 
+
+#Method 1:
+#Addition 
+add=5+4
+print("Addition :%d"%add)
+#Substraction 
+sub=5-2
+print("Substraction :%d"%sub)
+#Multiplication
+mul=5*10
+print("Multiplication %d"%mul)
+# Division
+div=10/2 
+print("Division :%.2f"%div)
