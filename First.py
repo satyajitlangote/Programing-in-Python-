195e338c7a5a2a02246595d0029cd383acc1d29d
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-19 19:46:21 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,10 @@
+# 1. Program to display name and address  
+
+    name="Satyajit"
+    address="Kokale,Sangli,Maharashtra,India"
+    print("Name:-{}\nAddress:{}".format(name,address))
+    
+
+
+
+    
