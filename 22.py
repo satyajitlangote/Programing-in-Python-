c3447c553bd16d3d39a8f56f6ad183c77e050cca
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-21 18:50:03 +0530

    Add files via upload

---
+++ 22.py
@@ -0,0 +1,7 @@
+def average(*t):
+    avg=sum(t)/len(t)
+    return avg
+result1=average(32,5,565,22,87,34,2,57)
+result2=average(5,10,15,20,25,30,35,40,45,50)
+print("Average is:",result1)
+print("Average is:",result2)
