b2f384674345b9bed9048da720b858c581e5a4fd
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-22 22:19:25 +0530

    Add files via upload

---
+++ TypeConversion.py
@@ -0,0 +1,13 @@
+# Type Conversion in Basics
+
+x=int('123')
+a=float('123.42')
+b=complex('3+4j')
+c=str(12)
+d=bool('True')
+e=bin(25)
+f=oct(25)
+g=hex(25)
+h=ord('A')
+i=chr(98)
+print(x,a,b,c,d,e,f,g,h,i,sep="\n")
