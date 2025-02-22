6819a8d2f38147506209a1a036abe6533692dfb2
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-22 22:08:50 +0530

    Add files via upload

---
+++ Read3.py
@@ -0,0 +1,14 @@
+n=15
+number_of_quesses=1
+print("Number of quesses is limited to only a times ")
+while(number_of_quesses <=9):
+    quesses_number=int(input("guesses the number "))
+    if guesses_number < 18:
+        print("You enter less number pleasee input greater number \n ")
+        elif guesses_number > 18:
+        print("you enter greater number please input smaller number \n")
+    else:
+        print("YOU WON \n")
+        print(number_of_guesse)
+
+
