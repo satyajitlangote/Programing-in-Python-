9b7aa40be81f3de441d6bfe258fbf19e8e579f63
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-02-24 23:25:50 +0530

    Add files via upload

---
+++ Module2.py
@@ -0,0 +1,12 @@
+print("Enter the 1st number \n")
+num1=int(input())
+print("Enter the 2nd number \n")
+num2=int(input())
+add=num1+num2
+print("Addition :%d"%add)
+sub=num1-num2
+print("Substraction :%d"%sub)
+mult=num1*num2
+print("Multiplication :%d"%mult)
+div=num1/num2
+print("Division :%d"%div)
