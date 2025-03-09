eedbb6aeab5901515f12b21394ea05e0c36a30cd
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-03-09 22:55:53 +0530

    Add files via upload

---
+++ Program2/3.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    return 1 
+if(n==1 or n==00):
+    else:
+        n*factorial(n-1)
+num=5
+print("Factorial of ",num,"is".factorial(num))
+
+        
---
+++ Program2/Module1.py
@@ -0,0 +1,13 @@
+# Program to Accept two number and display addition, subtraction, 
+#multiplication, division and modules.
+
+#Method1
+add=5+2
+print("Addition :%d"%add)
+sub=5-3
+print("Substraction:%d"%sub)
+mult=5*10
+print("Multiplication:%d"%mult)
+div=10/2
+print("Division :%.2f"%div)
+
