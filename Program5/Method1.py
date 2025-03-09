c6a6aa0d36b54d0cd5051ed4fc77b5431ac84f9d
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-03-09 22:57:04 +0530

    Add files via upload

---
+++ Program5/Method1.py
@@ -0,0 +1,13 @@
+# Program to display maximum number and minimum number from given list
+
+# Method 1
+
+test_list1=[1,3,4,5,2,6]
+test_list2=[3,4,8,3,10,1]
+print("The original list 1 is :"+str(test_list1))
+print("The original list 2 is:"+str(test_list2))
+max_all=max(test_list1+test_list1)
+min_all=min(test_list1+test_list2)
+
+print("The maximum of both lists is:"+str(max_all))
+print("The minimum of both lists is :"+str(min_all))
---
+++ Program5/Method2.py
@@ -0,0 +1,6 @@
+#Method2
+
+list1=[1,3,4,5,2,6];
+list2=[3,4,8,3,10,1]
+x=[i for i in list1 + list2]
+print("Maximum elements is\n",max(x),"\nminimum element is ",min(x))
