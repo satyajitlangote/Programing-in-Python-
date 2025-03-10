594adbe6ada675592b10a27db74671542a7c0686
Satyajit Langote <159507085+satyajitlangote@users.noreply.github.com>
2025-03-10 23:20:28 +0530

    Add files via upload

---
+++ Program9.py
@@ -0,0 +1,7 @@
+# Program to Today’s Year, Month, and Date
+from datetime import date
+todays_date=date.today()
+print("Current date ",todays_date)
+print("Current Year :",todays_date.year)
+print("Current month :",todays_date.month)
+print("Current day :",todays_date.day)
